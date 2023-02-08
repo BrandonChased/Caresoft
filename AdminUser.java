86ec976398925ae8453098a73276b48362621eb8
Brandon DeBenedictis <brandonchasedebenedictis@gmail.com>
2023-02-08 14:50:28 -0800

    CareSoft

---
+++ AdminUser.java
@@ -0,0 +1,62 @@
+import java.io.Serial;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AdminUser extends User implements HIPAACompliantAdmin, HIPAACompliantUser {
+    // ... imports class definition...
+    private String role;
+    private ArrayList<String> securityIncidents = new ArrayList<>();
+
+    public AdminUser(Integer id, String role) {
+        super(id);
+        this.role = role;
+        // TODO Auto-generated constructor stub
+    }
+    // Inside class:
+
+    // TO DO: Implement a constructor that takes an ID and a role
+    // TO DO: Implement HIPAACompliantUser!
+    // TO DO: Implement HIPAACompliantAdmin!
+
+    public void newIncident(String notes) {
+        String report = String.format(
+                "Datetime Submitted: %s \n,  Reported By ID: %s\n Notes: %s \n",
+                new Date(), this.id, notes);
+        securityIncidents.add(report);
+    }
+
+    public void authIncident() {
+        String report = String.format(
+                "Datetime Submitted: %s \n,  ID: %s\n Notes: %s \n",
+                new Date(), this.id, "AUTHORIZATION ATTEMPT FAILED FOR THIS USER");
+        securityIncidents.add(report);
+    }
+
+    @Override
+    public boolean assignPin(int pin) {
+        int length = String.valueOf(pin).length();
+        if (length >= 6) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean accessAuthorized(Integer confirmedAuthID) {
+        if( confirmedAuthID == this.getId()) {
+            return true;
+        }else {
+            this.authIncident();
+            return false;
+        }
+    }
+
+    @Override
+    public ArrayList<String> reportSecurityIncidents() {
+        // TODO Auto-generated method stub
+        return  securityIncidents;
+    }
+
+    // TO DO: Setters & Getters
+
+}
